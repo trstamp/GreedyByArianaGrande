cf1c6c3591982e80e705d6cca95d2deabdf3f5ac
Tyler <35055803+trstamp@users.noreply.github.com>
2020-11-29 15:41:48 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        Assignment4 assn4 = new Assignment4();
+        assn4.addToList();
+        assn4.addListToArray();
+        System.out.print(assn4.algorithm());
+    }
+}
